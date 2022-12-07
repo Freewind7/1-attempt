dadd3d0a138061792edcb0babe5b644ec946e612
Freewind7 <jenek777@ukr.net>
2022-12-07 23:38:46 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+import { AppRegistry } from "react-native"
+import App from "./screens/authorization"
+import {name as appName} from "./autoservice.json"
+
+AppRegistry.registerComponent (appName, () => App) 
---
+++ screens/authorization.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { StyleSheet, Text, View, TextInput } from "react-native";
+
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.description}>Будь ласка введіть свої облікові данні</Text>
+      <TextInput style={styles.input} keyboardType = "phone-pad"  placeholder ="Номер телефону"/>
+      <TextInput style={styles.input} secureTextEntry = "true" placeholder ="Пароль" />
+    </View>
+  );
+}
+
+
+// Стилі
+const styles = StyleSheet.create({
+  //Основний контейнер 
+  container : {
+    backgroundColor : "#AD8AF0",
+    position : "absolute",
+    width : "100%",
+    height : "100%", 
+  },
+  // Текст над полями
+  description : {
+    position : "absolute",
+    top : "35%",
+    left : "20%",
+  },
+  //Поля вводу
+  input: {
+  backgroundColor: "white",
+  width :"50%",
+  top : "40%",
+  left : "25%",
+  borderWidth : 1,
+  borderRadius : 10,
+  padding : 10,
+  margin : 10,
+  },
+});
+
+// Функції
+
+
